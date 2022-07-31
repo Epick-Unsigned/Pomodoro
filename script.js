800ade0a839f2b318c1e529c50eba01d21fa276a
none <none>
2022-07-31 15:10:47 -0300

    algo de barra terminado

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const UN_SEGUNDO_INTERVAL = 200;
+const UN_SEGUNDO_INTERVAL = 100;
 
 const UN_MIN = 60;
-const SESION_MIN = 25 * UN_MIN;
-const DESCANSO_MIN = 5 * UN_MIN;
+const SESION_MIN = 25 ;
+const DESCANSO_MIN = 5 ;
 
-let tiempoRegistrado = 0;
+
+
+
+//Valores por defecto para sesion
+const SEGUNDO_INICIAL = 0;
+const MINUTO_INICIAL = SESION_MIN-1;
+
+const SESION_NO_TERMINADA = false;
+const SESION_TERMINADA = true;
+const ESTADO_SESION_INICIAL = SESION_NO_TERMINADA;
+
+
 let idIntervalo = null;
+let terminoSesion = SESION_NO_TERMINADA;
+
+document.querySelector("#texto-barra").innerHTML = SESION_MIN+":0"+SEGUNDO_INICIAL;
 /**
- * Inicia una sesion pomodoro 
+ * Inicia estudio
  */
 function comenzarEstudio(){
     if(!idIntervalo){
@@ -17,33 +31,67 @@ function comenzarEstudio(){
     }
 }
 
+/**
+ * Inicia una sesion pomodoro
+ */
 function iniciarSesion(){
-    let seg = 0;
-    let min = 0;
+    let seg = SEGUNDO_INICIAL;
+    let min = MINUTO_INICIAL;
     console.log("iniciando sesion");
         idIntervalo = setInterval(()=>{
-            if(tiempoRegistrado==SESION_MIN){
-                console.log("pasaron 5 seg",idIntervalo);
+            if(terminoSesion){
                 clearInterval(idIntervalo);
             }else{
-                tiempoRegistrado++;
-                console.log("paso 1 seg");
                 seg = obtenerSegundos(seg);
-                document.querySelector("#texto-barra").innerHTML = `00:${darFormatoTxtSegundo(seg)}`;
+                modificarBarraProgreso(seg,min); 
+                min = obtenerMinutos(seg, min);
             }
         }, UN_SEGUNDO_INTERVAL);
 }
 
+function modificarBarraProgreso(seg, min){
+    // 25*60 -> 100
+    // x*x -> x*x*100/25*60
+    let progresoPorcentaje = 100 - (((seg + min*60 ) * 100) / (25*60));
+    console.log(progresoPorcentaje.toString()+"%", seg ,min);
+    (document.querySelector(".barra-interna")).style.width = progresoPorcentaje.toString()+"%";
+    document.querySelector("#texto-barra").innerHTML = `${darFormatoTxtMin(min)}:${darFormatoTxtSegundo(seg)}`;
+
+}
+
+/**
+ * Modifica los segundos dependiendo si paso o no un minuto
+ * @param {Number} seg actuales de sesion
+ * @returns segundos correspondientes
+ */
 function obtenerSegundos(seg){
-    if( seg == 60 ){
-        seg = 0;
-        console.log("paso un min");
+    if( seg == 0 ){
+        seg = 59;
     }else{
-        seg++;
-        console.log("paso un seg");
+        seg--;
     }
     return seg;
 }
+
+/**
+ * Modifica el valor de minutos dependiendo del valor de segundos y la cantidad de minutos por sesion
+ * @param {Number} seg actuales
+ * @param {Number} min actuales
+ * @return  minutos correspondientes de sesion
+ */
+function obtenerMinutos(seg, min){
+    if((min==0) && (seg==0)){
+        //termino sesion
+        console.log(terminoSesion);
+        terminoSesion = SESION_TERMINADA;
+        console.log(terminoSesion);
+        /* seg = 0; */
+    }else if(seg == 0){
+        min--;
+    }
+    return min;
+}
+
 
 /**
  * Devuelve los segundos en formato de texto 00 segun corresponda
@@ -58,4 +106,14 @@ function darFormatoTxtSegundo(seg){
         segTxt = "0"+seg;
     }
     return segTxt;
+}
+
+function darFormatoTxtMin(min){
+    let minTxt = "00";
+    if(min < 10){
+        minTxt = "0"+min;
+    }else{
+        minTxt = min.toString();
+    }
+    return minTxt
 }
